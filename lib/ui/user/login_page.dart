7e213138f4b50bd9355572856c3b82e7da066500
Your NameAnas-Nabih <anas7nabih@gmail.com>
2022-11-21 00:02:55 +0200

    handle user process (login and register account).

--- lib/ui/user/login_page.dart
+++ lib/ui/user/login_page.dart
@@ -1,70 +1,126 @@
+import 'package:chat_app/commanUtils/const.dart';
 import 'package:chat_app/commanUtils/navigator_uutils.dart';
+import 'package:chat_app/commanUtils/utils.dart';
 import 'package:chat_app/res/colors.dart';
 import 'package:chat_app/ui/user/register_page.dart';
 import 'package:chat_app/widgets/custom_button.dart';
 import 'package:chat_app/widgets/custom_text_form_field.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:sizer/sizer.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   LoginPage({Key? key}) : super(key: key);
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String? email, password;
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor:MColors.primaryColor,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 5.w),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Chat App",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18.sp,
-                  fontWeight: FontWeight.w400),
-            ),
-            SizedBox(height: 8.h),
-            Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    CustomTextFormField(hintText: "Email"),
-                    SizedBox(height: 4.h),
-                    CustomTextFormField(hintText: "Password")
-                  ],
-                )),
-            SizedBox(height: 8.h),
-            CustomBtn(text: "Sigin in"),
-            SizedBox(height: 4.h),
-            GestureDetector(
-              onTap: ()=>NavigatorUtils.push(context, RegisterPage()),
-              child: Text.rich(TextSpan(
-                text:"don't have an account?\t" ,
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        backgroundColor: MColors.primaryColor,
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 5.w),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Chat App",
                 style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 11.sp,
-                  fontWeight: FontWeight.w400),
-                children: [
-                  TextSpan(text: "Sigin Up",style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 12.sp,
-                      fontWeight: FontWeight.w600))
-                  ]
-              )),
-            ),
-
-          ],
+                    color: Colors.white,
+                    fontSize: 18.sp,
+                    fontWeight: FontWeight.w400),
+              ),
+              SizedBox(height: 8.h),
+              Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      CustomTextFormField(
+                          onChanged: (val) => email = val,
+                          validation: (val) {
+                            if (val!.isEmpty) {
+                              return "Please Enter Your Email";
+                            }
+                          },
+                          hintText: "Email"),
+                      SizedBox(height: 4.h),
+                      CustomTextFormField(
+                          onChanged: (val) => password = val,
+                          validation: (val) {
+                            if (val!.isEmpty) {
+                              return "Please Enter Your Password";
+                            } else if (val!.length < 6) {
+                              return "Password at least should be +6 chars";
+                            }
+                          },
+                          hintText: "Password")
+                    ],
+                  )),
+              SizedBox(height: 8.h),
+              CustomBtn(text: "Sigin in",onTapped:  () async {
+                if(_formKey.currentState!.validate()){
+                  setState(() {
+                    isLoading = true;
+                  });
+                  try{
+                    await loginWithEmailAndPassword(context);
+                  }on FirebaseAuthException catch(e){
+                    loginException(e, context);
+                  }catch(e){
+                    Utils.showSnackBar(context: context, msg:e.toString());
+                  }
+                 }
+                setState(() {
+                  isLoading = false;
+                });
+              }),
+              SizedBox(height: 4.h),
+              GestureDetector(
+                onTap: () => NavigatorUtils.push(context, RegisterPage()),
+                child: Text.rich(TextSpan(
+                    text: "don't have an account?\t",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 11.sp,
+                        fontWeight: FontWeight.w400),
+                    children: [
+                      TextSpan(
+                          text: "Sigin Up",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12.sp,
+                              fontWeight: FontWeight.w600))
+                    ])),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+
+  Future<void> loginWithEmailAndPassword(BuildContext context) async {
+    UserCredential user = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email!, password: password!);
+    print(user.user!.email);
+    Utils.showSnackBar(context: context, msg: "login Successfully");
+  }
+
+  void loginException(FirebaseAuthException e, BuildContext context) {
+     if (e.code == CONST.userNotFound) {
+      Utils.showSnackBar(context: context, msg: 'No user found for that email.');
+     } else if (e.code == CONST.wrongPassword) {
+      Utils.showSnackBar(context: context, msg: 'Wrong password provided for that user.');
+    }
+  }
 }
-
-
-
-
--- lib/ui/user/register_page.dart
+++ lib/ui/user/register_page.dart
@@ -7,92 +7,109 @@ import 'package:chat_app/widgets/custom_text_form_field.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+ import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:sizer/sizer.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   RegisterPage({Key? key}) : super(key: key);
 
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   String? email, password;
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: MColors.primaryColor,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 5.w),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Chat App",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18.sp,
-                  fontWeight: FontWeight.w400),
-            ),
-            SizedBox(height: 8.h),
-            Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    CustomTextFormField(
-                        onChanged: (val) => email = val,
-                        validation: (val){
-                          if(val!.isEmpty) {
-                            return "Please Enter Your Email";
-                          }
-                        },
-                        hintText: "Email"),
-                    SizedBox(height: 4.h),
-                    CustomTextFormField(
-                        onChanged: (val) => password = val,
-                        validation: (val){
-                          if(val!.isEmpty) {
-                            return "Please Enter Your Password";
-                          }else if(val!.length < 6){
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        backgroundColor: MColors.primaryColor,
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 5.w),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Chat App",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.sp,
+                    fontWeight: FontWeight.w400),
+              ),
+              SizedBox(height: 8.h),
+              Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      CustomTextFormField(
+                          onChanged: (val) => email = val,
+                          validation: (val){
+                            if(val!.isEmpty) {
+                              return "Please Enter Your Email";
+                            }
+                          },
+                          hintText: "Email"),
+                      SizedBox(height: 4.h),
+                      CustomTextFormField(
+                          onChanged: (val) => password = val,
+                          validation: (val){
+                            if(val!.isEmpty) {
+                              return "Please Enter Your Password";
+                            }else if(val!.length < 6){
+                              return "Password at least should be +6 chars";
+                            }
+                          },
+                          hintText: "Password")
+                    ],
+                  )),
+              SizedBox(height: 8.h),
+              CustomBtn(
+                  text: "Sigin Up",
+                  onTapped: () async {
+                    if(_formKey.currentState!.validate()){
+                      setState(() {
+                        isLoading = true;
+                      });
+                      try{
+                        await registerUserWithEmailAndPassword();
+                      }on FirebaseAuthException catch(e){
+                        registerException(e, context);
+                       }catch (e){
 
-                          }return "Password at least should be +6 chars";
-                        },
-                        hintText: "Password")
-                  ],
-                )),
-            SizedBox(height: 8.h),
-            CustomBtn(
-                text: "Sigin Up",
-                onTapped: () async {
-                  if(_formKey.currentState!.validate()){
-                    try{
-                      await registerUserWithEmailAndPassword();
-                      Utils.showSnackBar(context: context, msg: "Login Successfully");
-                    }on FirebaseAuthException catch(e){
-                      registerException(e, context);
-                    }catch (e){
-                      Utils.showSnackBar(context: context, msg:e.toString());
+                        Utils.showSnackBar(context: context, msg:e.toString());
+                      }
                     }
-                  }
 
-                }),
-            SizedBox(height: 4.h),
-            GestureDetector(
-              onTap: () => NavigatorUtils.pop(context),
-              child: Text.rich(TextSpan(
-                  text: "already have an account?\t",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 11.sp,
-                      fontWeight: FontWeight.w400),
-                  children: [
-                    TextSpan(
-                        text: "Sigin in",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.w600))
-                  ])),
-            ),
-          ],
+                    setState(() {
+                      isLoading = false;
+                    });
+
+                  }),
+              SizedBox(height: 4.h),
+              GestureDetector(
+                onTap: () => NavigatorUtils.pop(context),
+                child: Text.rich(TextSpan(
+                    text: "already have an account?\t",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 11.sp,
+                        fontWeight: FontWeight.w400),
+                    children: [
+                      TextSpan(
+                          text: "Sigin in",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12.sp,
+                              fontWeight: FontWeight.w600))
+                    ])),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -110,5 +127,8 @@ class RegisterPage extends StatelessWidget {
      UserCredential user = await FirebaseAuth.instance
         .createUserWithEmailAndPassword(
         email: email!, password: password!);
+     Utils.showSnackBar(context: context, msg: "Login Successfully");
+
   }
+
 }
